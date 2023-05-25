b7d8dc83d3fc70482b404fbe49be5a3dc7d3bfd7
adiiv <adivered3@gmail.com>
2023-05-25 19:46:07 +0300

    Updated

--- src/QuizCreator/main.java
+++ src/QuizCreator/main.java
@@ -4,12 +4,6 @@ import java.io.IOException;
 public class main {
 
 	public static void main(String[] args) throws IOException {
-		/*
-		 * Question Bank Project
-		 * Questions bank is saved in text file, can be loaded if such text file exists
-		 * Adi Vered, Guy Ben Moshe
-		 * 318243755, 318363397
-		 */
 		Menu menu = new Menu();
 		menu.runMainMenu();
 
